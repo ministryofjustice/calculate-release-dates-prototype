63ad5ff41e36adfa8462dc4d06ebb2f86937a8be
Ashley Wheat <ashleywheat@ashleys-mbp.mynet>
2021-07-21 23:52:29 +0100

    Added sandbox. Added alternatate versions of various pages.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/sandbox/offence-days-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'juggling-balls'
+  var offenceDays = req.session.data['offence-multiple-days-yes']
+console.log(offenceDays)
+  // Check whether the variable matches a condition
+  if (offenceDays == "yes"){
+    // Send user to next page
+    res.redirect('/sandbox/offence-date-a-3')
+  }
+
+})
+
 module.exports = router
