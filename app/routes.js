f58318a9e9c4526a362e40e4669f2af8f7f85e1c
Ashley Wheat <ashleywheat@Ashleys-MacBook-Pro.local>
2021-07-29 09:03:33 +0100

    Version 2. Added adjustemnts journey. Removed some pages.

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
+// Run this code when a form is submitted 
 router.post('/sandbox/offence-days-answer', function (req, res) {
 
-  // Make a variable and give it the value from 'juggling-balls'
+  // Make a variable and give it the value 
   var offenceDays = req.session.data['offence-multiple-days']
 console.log(offenceDays)
   // Check whether the variable matches a condition
@@ -14,6 +14,41 @@ console.log(offenceDays)
     // Send user to next page
     res.redirect('/sandbox/offence-date-a-3')
   }
+
+})
+
+// Route for adjustments
+router.post('/version-2/adjustments-answer', function (req, res) {
+
+  // Make a variable and give it the value 
+  var adjustmentsApply = req.session.data['are-there-any-adjustments']
+console.log(adjustmentsApply)
+  // Check whether the variable matches a condition
+  if (adjustmentsApply == "yes"){
+    // Send user to next page
+    res.redirect('/version-2/what-adjustments') 
+  } else res.redirect('/version-2/check-answers')
+
+})
+
+// Route for adjustments
+router.post('/version-2/what-adjustments-apply', function (req, res) {
+
+  var adjustment = req.session.data['adjustment']
+  var nextQ = req.session.data['nextQ']
+
+console.log(adjustment)
+  if (adjustment == "remand"){
+    res.redirect('/version-2/remand-dates') 
+  }
+  else if (adjustment == "tagged-bail"){
+    res.redirect('/version-2/tagged-bail-dates')
+  }
+  else if (adjustment == 'remand', 'tagged-bail'){
+    console.log("remand + tagged bail")
+    res.redirect("/version-2/remand-dates-2")
+  }
+  else res.redirect('/version-2/check-answers')
 
 })
 
