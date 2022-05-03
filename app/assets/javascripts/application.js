ddfdab7634c5bbeb6603ad0ea3584a2993192e06
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2022-05-03 11:46:13 +0100

    v8: changes to SDS+ journey

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,17 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
   window.MOJFrontend.initAll()
 })
+
+// Check all checkboxes
+$('#select-all').click(function(event) {   
+    if(this.checked) {
+        // Iterate each checkbox
+        $(':checkbox').each(function() {
+            this.checked = true;                        
+        });
+    } else {
+        $(':checkbox').each(function() {
+            this.checked = false;                       
+        });
+    }
+}); 
