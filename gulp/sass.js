992c9a19c6acbdebe62d2065a629b19d96381deb
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2022-06-10 11:37:37 +0100

    committing after updating prototype kit

--- gulp/sass.js
+++ gulp/sass.js
@@ -6,8 +6,7 @@
 */
 
 const gulp = require('gulp')
-const sass = require('gulp-sass')
-const sourcemaps = require('gulp-sourcemaps')
+const sass = require('gulp-sass')(require('sass'))
 const path = require('path')
 const fs = require('fs')
 
@@ -23,9 +22,11 @@ gulp.task('sass-extensions', function (done) {
 })
 
 gulp.task('sass', function () {
-  return gulp.src(config.paths.assets + '/sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', function (error) {
+  return gulp.src(config.paths.assets + '/sass/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({
+      outputStyle: 'expanded',
+      quietDeps: true
+    }).on('error', function (error) {
       // write a blank application.css to force browser refresh on error
       if (!fs.existsSync(stylesheetDirectory)) {
         fs.mkdirSync(stylesheetDirectory)
@@ -34,31 +35,30 @@ gulp.task('sass', function () {
       console.error('\n', error.messageFormatted, '\n')
       this.emit('end')
     }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(stylesheetDirectory))
+    .pipe(gulp.dest(stylesheetDirectory, { sourcemaps: true }))
 })
 
 gulp.task('sass-documentation', function () {
-  return gulp.src(config.paths.docsAssets + '/sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.paths.public + '/stylesheets/'))
+  return gulp.src(config.paths.docsAssets + '/sass/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({
+      outputStyle: 'expanded',
+      quietDeps: true
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(config.paths.public + '/stylesheets/', { sourcemaps: true }))
 })
 
 // Backward compatibility with Elements
 
 gulp.task('sass-v6', function () {
-  return gulp.src(config.paths.v6Assets + '/sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
+  return gulp.src(config.paths.v6Assets + '/sass/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({
       outputStyle: 'expanded',
       includePaths: [
         'node_modules/govuk_frontend_toolkit/stylesheets',
         'node_modules/govuk-elements-sass/public/sass',
         'node_modules/govuk_template_jinja/assets/stylesheets'
-      ]
+      ],
+      quietDeps: true
     }).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.paths.public + '/v6/stylesheets/'))
+    .pipe(gulp.dest(config.paths.public + '/v6/stylesheets/', { sourcemaps: true }))
 })
